c004fa16cb7b0aa1209a1031243677c06063b207
Mayko Rossi <maykorossi@hotmail.com>
2024-06-03 22:02:03 -0300

    concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("SmartPhone Nokia");
+        Smartphone nokia = new Nokia(numero: "777777", modelo: "Tijolao", imei: "1111", memoria: 256);
+        Console.WriteLine($"Numero: {nokia.Numero}, Modelo: {nokia.Modelo}, IMEI: {nokia.Imei}, Memoria: {nokia.Memoria}GB");
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Telegram");
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("SmartPhone Iphone");
+        Smartphone iphone = new Iphone(numero: "999999", modelo: "XR", imei: "2222", memoria: 512);
+        Console.WriteLine($"Numero: {iphone.Numero}, Modelo: {iphone.Modelo}, IMEI: {iphone.Imei}, Memoria: {iphone.Memoria}GB");
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("WhatsApp");
+    }
+}
